c9bb32e98eb4fd79797e50279d1d5ec77e30aec7
liam-allman <84319148+liam-allman@users.noreply.github.com>
2024-04-03 18:34:13 +0100

    updates to gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,50 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
-const connectPhp = require('gulp-connect-php');
-const browserSync = require('browser-sync').create();
-const browserify = require('browserify');
-const source = require('vinyl-source-stream');
+const cleanCSS = require('gulp-clean-css');
 
-// 
-// FIX JS IN FILE
-// 
+const paths = {
+  styles: {
+    src: 'scss/src/*.scss',
+    dest: 'scss/dist/'
+  },
+  scripts: {
+    src: 'js/src/*.js',
+    dest: 'js/dist/'
+  }
+};
 
-// Task to minify and bundle JavaScript using Browserify
-gulp.task('minify-js', function () {
-  return browserify('./js/src/main.js') // Entry point for your JavaScript files
-    .bundle()
-    .pipe(source('bundle.js')) // Output filename after bundling
-    .pipe(uglify()) // Minify the bundled JavaScript
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./js/dist/'));
-});
-
-// Task to compile SCSS to CSS and minify
-gulp.task('minify-css', function () {
-  return gulp.src('./scss/src/*.scss')
+function styles() {
+  return gulp.src(paths.styles.src)
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./scss/dist/'));
-});
+    .pipe(rename({
+      basename: 'global',
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.styles.dest)); // Output to a new directory
+}
 
-// Task to start PHP server and watch for changes
-gulp.task('serve', function(done) {
-  connectPhp.server({ base: './', port: 8100 }, function (){
-    gulp.watch('./js/src/*.js', gulp.series('minify-js'));
-    gulp.watch('./scss/src/*.scss', gulp.series('minify-css'));
-    
-    // Watch for changes in all files and trigger browser reload
-    gulp.watch(['./**/*.php', './js/src/*.js', './scss/src/*.scss']).on('change', browserSync.reload);
+function scripts() {
+  return gulp.src(paths.scripts.src, { sourcemaps: true })
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat('global.min.js'))
+    .pipe(gulp.dest(paths.scripts.dest)); // Output to a new directory
+}
 
-    done();
-  });
-});
+function watch() {
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.styles.src, styles);
+}
 
-// Task to initialize BrowserSync and proxy it to PHP server
-gulp.task('browser-sync', function() {
-  browserSync.init({
-    proxy: '127.0.0.1:8100', // Assuming your PHP server is running on localhost port 8100
-    port: 3000, // BrowserSync port
-  });
-});
+const build = gulp.parallel(styles, scripts); // No need for clean in build task
 
-// Default task to start both the PHP server and BrowserSync
-gulp.task('default', gulp.series('serve', 'browser-sync', 'minify-js', 'minify-css'));
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watch;
+exports.build = build;
+exports.default = build;
